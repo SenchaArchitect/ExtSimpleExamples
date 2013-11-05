98e674d4b86f1fde2b3134e8260471b505567c87
Arne Bech <arne@sencha.com>
2013-11-05 11:06:24 -0700

    Updated checkTree

---
+++ checkTree/app/Application.js
@@ -0,0 +1,17 @@
+Ext.define('checkTree.Application', {
+    name: 'checkTree',
+
+    extend: 'Ext.app.Application',
+
+    views: [
+        // TODO: add views here
+    ],
+
+    controllers: [
+        // TODO: add controllers here
+    ],
+
+    stores: [
+        // TODO: add stores here
+    ]
+});
---
+++ checkTree/app/controller/Main.js
@@ -0,0 +1,3 @@
+Ext.define('checkTree.controller.Main', {
+    extend: 'Ext.app.Controller'
+});
